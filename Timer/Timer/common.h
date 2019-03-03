bfab6ffff95434b1526f1bc3c6ded9a58eafca16
mohamedmounirr <48018295+mohamedmounirr@users.noreply.github.com>
2019-03-03 23:36:03 +0200

    TIMER

---
+++ Timer/Timer/common.h
@@ -0,0 +1,22 @@
+/*
+ * common.h
+ *
+ * Created: 2/26/2019 2:00:20 PM
+ *  Author: AG PC
+ */ 
+
+
+
+#define COMMON_H_
+#define NULL ((uint_8*)0)
+typedef enum
+{
+	TRUE = 0 ,
+	FALSE ,
+}tenum_TF;
+typedef unsigned char uint_8 ;
+typedef unsigned short int uint_16 ;
+typedef unsigned long  int uint_32 ;
+typedef signed short int int_16 ;
+typedef signed long  int int_32 ;
+
